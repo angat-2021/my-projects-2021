b150e5826e08eb39a655a9bfc67445af32e08ae0
Angat Yogi <angat.yogi@ou.edu>
2021-02-27 15:09:30 -0600

    AddedNewClass

---
+++ Patterns_Try/Try.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns_Try
+{
+    static class Try
+    {
+       // Console.WriteLine("I hope adding another class also works to push in github");
+    }
+}
