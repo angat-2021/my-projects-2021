58c7b328865d59f7360d34e39749713033b5f888
Angat Yogi <angat.yogi@ou.edu>
2021-02-27 15:13:30 -0600

    AnotherSolutionAddedToGithub

---
+++ AnotherProject-Solution/AnotherProject-Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnotherProject_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
