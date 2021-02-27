90d5ccdd2069d554df97dd27b1e4fd98c50e9ec7
Angat Yogi <angat.yogi@ou.edu>
2021-02-27 15:00:32 -0600

    Add project files.

---
+++ Patterns_Try/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns_Try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
